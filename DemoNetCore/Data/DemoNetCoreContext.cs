b6da4d56b3ab0f1d95c5c52a78c45b611cff3269
vuquangmt <vuquang11gbg@gmail.com>
2021-09-23 16:55:21 +0700

    Add project files.

---
+++ DemoNetCore/Data/DemoNetCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoNetCore.Models;
+
+namespace DemoNetCore.Data
+{
+    public class DemoNetCoreContext : DbContext
+    {
+        public DemoNetCoreContext (DbContextOptions<DemoNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoNetCore.Models.Movie> Movie { get; set; }
+    }
+}
